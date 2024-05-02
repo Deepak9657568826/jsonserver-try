6b55f1a3a79d2fdf2f44133809a59501635a099a
Deepak Wayzode <dwayzode@gmail.com>
2024-05-02 14:49:23 +0530

    jsson server

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express")
-
+const mongoose  = require("mongoose")
 const app = express();
+app.use(express.json())
+try {
+    mongoose.connect("mongodb://localhost:27017/jsontry")
+    console.log("connectd to db");
+    
+} catch (error) {
+    console.log(error);
+}
+
+const userSchema = mongoose.Schema({
+    name:String, 
+    pass:String
+})
+
+const Usermodel = mongoose.model("users", userSchema)
+
+
+
+
+
 
 
 app.get("/", (req , res)=>{
     res.send("This is home page")
 })
 
-app.post("/login", (req , res)=>{
-    res.send("This is login page")
+app.post("/register", async(req , res)=>{
+    const payload = req.body
+
+    try {
+        const user = new Usermodel(payload)
+        await user.save();
+        res.send("user added succefully")
+        
+    } catch (error) {
+        res.send("Something went wrong")
+    }
 })
 
 
